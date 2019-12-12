f40575ea5d9630b9ebf57efb3ef2b2499f1ca9d1
Sebastien Gerard <sgerard@emasphere.com>
2019-12-12 08:32:34 +0100

    change theming

--- serve-build-before.js
+++ serve-build-before.js
@@ -1,5 +1,18 @@
 module.exports = function () {
     const fs = require('fs');
+    const fsEx = require('fs-extra')
+    var rimraf = require("rimraf");
+
+    const buildDirectory = 'build/resources/';
+
+
+    function isDirectory(pathItem) {
+        try {
+            return fs.statSync(pathItem).isDirectory();
+        } catch (e) {
+            return false;
+        }
+    }
 
     let theme;
     if (process.env.EMA_THEME) {
@@ -8,5 +21,14 @@ module.exports = function () {
         theme = "default";
     }
 
-    fs.copyFileSync('src/theme/' + theme + '/variables.scss', 'src/theme/variables.scss');
+    if (!isDirectory(buildDirectory)) {
+        rimraf.sync(buildDirectory);
+        fs.mkdirSync(buildDirectory, {recursive: true});
+    }
+
+    try {
+        fsEx.copySync('src/theme/' + theme, buildDirectory)
+    } catch (error) {
+        return console.error('Unable to scan directory. Are-you sure the theme exists? ' + error);
+    }
 };
